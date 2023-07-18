2c2c4b738913a377e8a14b03f3b0a27ad9089b45
Avineet Bhat <surajbhat316@gmail.com>
2023-07-18 22:04:31 +0530

    Added env file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const db = require('./config/mongoose');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 
 app.use(express.urlencoded());
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
+// require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerceAPI');
-
+// mongoose.connect('mongodb://127.0.0.1:27017/ecommerceAPI');
+mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error Connexion to DB"));
